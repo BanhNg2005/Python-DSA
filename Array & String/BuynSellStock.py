c25f7dc048a25d6ef1fafe0a12cd145d10314568
Bao Anh <nguyenlebaoanh05@gmail.com>
2024-09-07 12:55:18 -0400

    best time to buy and sell stock

--- Array & String/BuynSellStock.py
+++ Array & String/BuynSellStock.py
@@ -2,14 +2,15 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        max_profit = float('-inf')
-        for i in range(len(prices)):
-            for j in range(i+1, len(prices)):
+        max_profit = float('-inf') # use the built-in float negative infinity to compare with the profit
+        for i in range(len(prices)): # start of the buying point (buying index)
+            for j in range(i+1, len(prices)): # selling index starting at i + 1
                 profit = prices[j] - prices[i]
                 if profit > 0:
-                    max_profit = max(max_profit, profit)
-        return max_profit if max_profit > float('-inf') else 0
+                    max_profit = max(max_profit, profit)  # update max_profit if profit is greater
+        return max_profit if max_profit > float('-inf') else 0 
 
+# Test
 def main() -> None:
     prices = [6,1,5,2,6,4]
     sol = Solution()
