986e9d45216746a7b5e9c7ac59c87b59f54d3d30
Bao Anh <nguyenlebaoanh05@gmail.com>
2024-09-07 20:44:26 -0400

    Valid anagram or not

---
+++ Array & String/ValidAnagram.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        if len(s) != len(t):
+            return False
+
+        countS, countT = {}, {} # create hashmap
+
+        for i in range(len(s)): # let i iterate through the length of s
+            countS[s[i]] = 1 + countS.get(s[i], 0) 
+            # countS[s[i]] is the key of the hashmap of the value s at index i
+            # everytime we see the same key, we increment the value by 1 or 0 if it doesn't exist
+            # ** the get method is for hashmap where it will return the value of the key if it exists
+            # or it will just return the default value 
+            countT[t[i]] = 1 + countT.get(t[i], 0) # same thing but for t
+        for j in countS: # iterate through the hashmap to make sure the values are the same, j is the key
+            if countS[j] != countT.get(j, 0): 
+                # check if j exists in the T hashmap by using the get method and if the value is the same or not
+                return False   
+        return True
+    # Time complexity: O(n) or O(s + t) since we have to iterate through both of the strings
+    # Space complexity: O(1) or O(s + t) because it is up to the size of the s and t strings
+
+def main() -> None:
+    s = 'lmao'
+    t = 'maol'
+    sol = Solution()
+    print(sol.isAnagram(s,t))
+
+if __name__ == '__main__':
+    main()
