487bc2fd4ed5043b282f50088fd225d0e4df009f
Bao Anh <nguyenlebaoanh05@gmail.com>
2024-10-11 00:15:25 -0400

    Contains Duplicate v2

--- Hash Map/ContainsDuplicate.py
+++ Hash Map/ContainsDuplicate.py
@@ -3,17 +3,29 @@ from typing import List
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         # Hash table approach
-        dup_dict = {} # number : frequency
+        # dup_dict = {} # number : frequency
+        #
+        # for i in nums:
+        #     if i in dup_dict: # as it iterates through the list, if the number is found again, we can return true
+        #         return True
+        #     else:
+        #         dup_dict[i] = 1 # if the number is not found, add it to the dictionary
+        # return False
 
+        # Time complexity: O(n) where n is the number of elements in the list
+        # Space complexity: O(n) since we are storing the frequency of each element in the list
+
+        # Set approach
+        dup = set() # create a set to store the elements in the list
         for i in nums:
-            if i in dup_dict: # as it iterates through the list, if the number is found again, return True
+            if i in dup:
                 return True
             else:
-                dup_dict[i] = 1 # if the number is not found, add it to the dictionary
+                dup.add(i) # add the element to the set if it is not in the set yet
         return False
 
-    # Time complexity: O(n) where n is the number of elements in the list
-    # Space complexity: O(n) since we are storing the frequency of each element in the list
+        # Time complexity: O(n) where n is the number of elements in the list
+        # Space complexity: O(n) due to the storage of elements in the set
 
 
 def main() -> None:
